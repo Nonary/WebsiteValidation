f15f08edd205aa2e218ab383b5e5df70c21734a5
Chase Payne <chase_payne@outlook.com>
2018-12-29 13:24:47 -0600

    ABC

--- WebApplication1/Models/User.cs
+++ WebApplication1/Models/User.cs
@@ -1,19 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
-using Microsoft.Extensions.Primitives;
 
 namespace WebApplication1.Models
 {
-
     public class User
     {
         [Display(Name = "First Name")]
@@ -27,7 +18,7 @@ namespace WebApplication1.Models
 
         [Display(Name = "E-Mail Address")]
         [DataType(DataType.EmailAddress)]
-        [Remote(action: "ValidateEmailAddress", controller: "Home")]
+        [Remote("ValidateEmailAddress", "Home")]
         [Required]
         public string EmailAddress { get; set; }
 
@@ -38,38 +29,28 @@ namespace WebApplication1.Models
 
         [Required(ErrorMessage = "Please select a Country.")]
         public string Country { get; set; }
+
         [Required(ErrorMessage = "Please select a State or Providence.")]
 
         public string State { get; set; }
-
-
-
     }
 
     public class BirthdayValidationAttribute : ValidationAttribute
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
- 
-            if ((DateTime)value > DateTime.Now.AddYears(-100) && (DateTime)value < DateTime.Now)
-            {
+            if ((DateTime) value > DateTime.Now.AddYears(-100) && (DateTime) value < DateTime.Now)
                 return ValidationResult.Success;
-            }
-            else
-            {
-                return new ValidationResult(null);
-            }
+            return new ValidationResult(null);
         }
-
-        
- 
     }
 
-        public class Country
+    public class Country
     {
         public string Name { get; set; }
+
         public static List<string> GetCountries { get; set; }
-        = new List<string>
+            = new List<string>
             {
                 null,
                 "Canada",
@@ -81,5 +62,4 @@ namespace WebApplication1.Models
             return Name;
         }
     }
-
 }
